65dd3b44324a0de32813aa5fb1a58aad6c461b34
Ajith Kumar <ajithvnr2001@gmail.com>
2025-11-26 06:44:48 +0000

    Initial commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
+    """Return a friendly HTTP greetinsg."""
     return 'Hello World! Version 1'
 
 if __name__ == '__main__':
