f2602fd8c6a888e32abceaa2ed045edcb1ad216c
Ajith Kumar <ajithvnr2001@gmail.com>
2025-11-26 06:33:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World! Version 1'
+
+if __name__ == '__main__':
+    # App Engine use pannum pothu, ithu run aagathu.
+    # Gunicorn maari oru server thaan run pannum.
+    app.run(host='127.0.0.1', port=8080, debug=True)
